a99800283bb5329958752d0a16fe0151a727ea8f
mannverma13 <121688470+mannverma13@users.noreply.github.com>
2023-10-15 01:11:46 +0530

    #48 final submission update index file (#49)

--- main.tf
+++ main.tf
@@ -44,24 +44,25 @@ into four nations: the Water Tribes, the Earth Kingdom, the Fire Nation,
 and the Air Nomads.
 DESCRIPTION
   domain_name = module.home_avatar_hosting.domain_name
-  town = "missingo"
+  town = "video-valley"
   content_version = var.avatar.content_version
 }
-module "home_payday_hosting" {
-  source = "./modules/terrahome_aws"
-  user_uuid = var.teacherseat_user_uuid
-  public_path = var.payday.public_path
-  content_version = var.payday.content_version
-}
 
-resource "terratowns_home" "home_payday" {
-  name = "Making your Payday Bar"
-  description = <<DESCRIPTION
-Since I really like Payday candy bars but they cost so much to import
-into Canada, I decided I would see how I could my own Paydays bars,
-and if they are most cost effective.
-DESCRIPTION
-  domain_name = module.home_payday_hosting.domain_name
-  town = "missingo"
-  content_version = var.payday.content_version
-}
+#module "home_payday_hosting" {
+#  source = "./modules/terrahome_aws"
+#  user_uuid = var.teacherseat_user_uuid
+##  public_path = var.payday.public_path
+#  content_version = var.payday.content_version
+#}
+#
+#resource "terratowns_home" "home_payday" {
+#  name = "Making your Payday Bar"
+#  description = <<DESCRIPTION
+#Since I really like Payday candy bars but they cost so much to import
+#into Canada, I decided I would see how I could my own Paydays bars,
+#and if they are most cost effective.
+#DESCRIPTION
+#  domain_name = module.home_payday_hosting.domain_name
+#  town = "missingo"
+#  content_version = var.payday.content_version
+#}
